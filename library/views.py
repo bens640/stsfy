e12b7c8b2fc43dfb5c35ee9c36094ec5217a1534
TvChkr <bens640@gmail.com>
2021-03-16 11:14:31 -0400

    Added library app and added home view

---
+++ library/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+
+    return render(request, 'library/home.html')
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
